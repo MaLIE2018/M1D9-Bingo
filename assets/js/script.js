b1f88e338ed0ce1f38426d0d0ab34d37ee71f64d
Maximilian Liebsch <43824628+MaLIE2018@users.noreply.github.com>
2021-03-19 08:11:21 +0100

    finalized

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,42 @@
 let mainboard = document.getElementById("board")
 let userboards = document.getElementById("userboards")
 let availableNumber = []
+let userfieldNumbers = []
+let numberOfuserboardFields = 24
 let rounds = 0
+
+
+const FillArray = (count) => {
+    for (let i = 0; i < count; i++) {
+        userfieldNumbers.push(i)
+    }
+}
 
 const fillBingoField = (numOfFields, board, isUserBoard) => {
     for (let i = 1; i <= numOfFields; i++) {
         let field = document.createElement("div")
         let h3 = document.createElement("h3")
         if (isUserBoard) {
-            console.log("test");
-            for (let i = 0; i < 76; i++) {
-                availableNumber.push(i)
-            }
-            field.appendChild(h3).innerText = avoidRepetition()
+            FillArray(76)
+            field.appendChild(h3).innerText = avoidRepetition(userfieldNumbers)
             field.classList.add("field")
             board.appendChild(field)
+            userfieldNumbers = []
         } else {
             field.appendChild(h3).innerText = i
             field.classList.add("field")
             board.appendChild(field)
-
             availableNumber.push(i)
         }
     }
-
 }
 
 
 const tryYourLuck = () => {
-    let randomNum = avoidRepetition()
+    let randomNum = avoidRepetition(availableNumber)
     const fieldSelected = document.querySelector(".selected")
-    console.log('fieldSelected:', fieldSelected)
+
+
 
     document.getElementById("luckyNumber").innerText = randomNum
 
@@ -41,15 +47,41 @@ const tryYourLuck = () => {
     for (let i = 0; i < board.children.length; i++) {
         if (randomNum === parseInt(board.children[i].innerText)) {
             board.children[i].classList.add("selected")
-            for (const item of userboards) {
-                item.children[i].classList.add("selected")
+        }
+    }
+
+
+    for (let i = 0; i < userboards.children.length; i++) {
+        for (let j = 0; j < userboards.children[i].children.length; j++) {
+            if (randomNum === parseInt(userboards.children[i].children[j].innerText)) {
+                userboards.children[i].children[j].classList.add("selected")
             }
+        }
+    }
+
+    //WinLost situation
+    for (let i = 0; i < mainboard.children.length; i++) {
+        if (mainboard.children[i].getElementsByClassName("selected").length === 76) {
+            alert(`Sorry, you lost. New Game?`)
+            setTimeout(function() {
+                window.location.reload()
+            }, 300)
+        }
+    }
+
+    for (let i = 0; i < userboards.children.length; i++) {
+        if (userboards.children[i].getElementsByClassName("selected").length === numberOfuserboardFields) {
+            alert(`You won on board ${i+1} New Game?`)
+            setTimeout(function() {
+                window.location.reload()
+            }, 300)
         }
 
     }
-    console.log('availableNumber:', availableNumber)
+
 
 }
+
 
 const howManyRound = (params) => {
     rounds = prompt("How many boards you want to play?")
@@ -57,31 +89,31 @@ const howManyRound = (params) => {
 
 window.onload = function(params) {
 
-    //howManyRound()
+    howManyRound()
     fillBingoField(76, mainboard, false)
 
-    for (let i = 1; i <= 3; i++) {
+    for (let i = 1; i <= rounds; i++) {
         let div = document.createElement("div")
-        div.classList.add(`uboard${i}`)
+        div.classList.add("uboard")
         userboards.appendChild(div)
-        fillBingoField(24, div, true)
+        fillBingoField(numberOfuserboardFields, div, true)
     }
 }
 
 
-const avoidRepetition = () => {
+const avoidRepetition = (array) => {
     let stillAvailable = true
     let pos = false
     let randomNum = 0
-    if (availableNumber.length === 0) {
+    if (array.length === 0) {
         document.getElementById("luckyNumber").innerText = "End"
         return alert("That have been all the numbers!")
     }
     do {
         randomNum = Math.floor(Math.random() * 76 + 1)
-        pos = availableNumber.includes(randomNum)
+        pos = array.includes(randomNum)
         if (pos === true) { //
-            availableNumber.splice(availableNumber.indexOf(randomNum), 1)
+            array.splice(array.indexOf(randomNum), 1)
             stillAvailable = false
         }
 
